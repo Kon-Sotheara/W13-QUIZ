ad2022591f68854ca3bd20c951b2dcacce8ff762
Sotheara Kon <sothearakon73@gmail.com>
2025-12-25 10:04:16 +0700

    last push

---
+++ quiz/lib/ui/groceries/grocery_list.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/ui/groceries/search.dart';
+import '../../data/mock_grocery_repository.dart';
+import '../../models/grocery.dart';
+import 'grocery_form.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+enum GroceryListViewTab { groceries, search }
+
+class _GroceryListState extends State<GroceryList> {
+  GroceryListViewTab _currentTab = GroceryListViewTab.groceries;
+
+
+  void onCreate() async {
+    // Navigate to the form screen using the Navigator push
+    Grocery? newGrocery = await Navigator.push<Grocery>(
+      context,
+      MaterialPageRoute(builder: (context) => const GroceryForm()),
+    );
+    if (newGrocery != null) {
+      setState(() {
+        dummyGroceryItems.add(newGrocery);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (dummyGroceryItems.isNotEmpty) {
+      //  Display groceries with an Item builder and  LIst Tile
+      content = ListView.builder(
+        itemCount: dummyGroceryItems.length,
+        itemBuilder: (context, index) =>
+            GroceryTile(grocery: dummyGroceryItems[index]),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: onCreate, icon: const Icon(Icons.add))],
+      ),
+      body: IndexedStack(
+        index: _currentTab.index,
+        children: [
+          content,
+          Search()
+        ],
+        
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab.index,
+        onTap: (index) {
+          setState(() {
+            _currentTab = GroceryListViewTab.values[index];
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.local_grocery_store), label: 'Groceries'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search')
+        ]
+        ),
+    );
+  }
+}
+
+class GroceryTile extends StatelessWidget {
+  const GroceryTile({super.key, required this.grocery});
+
+  final Grocery grocery;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(width: 15, height: 15, color: grocery.category.color),
+      title: Text(grocery.name),
+      trailing: Text(grocery.quantity.toString()),
+    );
+  }
+}
